7f59f55eab4678efad6b5e956ac6925c21b255a8
JOHN TANGKOBO <55840870+gbologbolo@users.noreply.github.com>
2023-02-27 11:58:50 +0000

    Update John Tangkobo.py

--- John Tangkobo.py
+++ John Tangkobo.py
@@ -1,27 +1,31 @@
-#information about the programme for the user
-print("This programme will ask you for a number and print out the Time Table of that number (i.e. from 1 to 12)")
-print("NOTE: Please only whole numbers are allowed")
-
-#Asking the User for an input number
-user_number = float(input("Input any number you want: "))
-
-#Converting floating numbers to integer numbers
-user_number = int(user_number)
-
-print("Times Table of ["+ str(user_number) + "] is as follows")
-
-#Initialization of the starting point of the times table (from 1 to 12)
-times = int("1")
+number = float(input("Input any number you want "))
+num = int("1")
 
 #Condition for the iteration...
-while (times <= 12):
-    #performing arthematics
-    answer = user_number * times
-    
-    #displaying the times table of the user input number
-    print(str(user_number) + " by " + str(times) + " = " + str(answer))
 
-    #Increamenting the initiazation value
-    times = times+1
-else:
-    print("Thank you!")
+if number.is_integer():
+    while (num < 13):
+        #performing arthematics
+        number = int(number)
+        answer = number * num
+        answer = int(answer)
+        #displaying the times table of the user input number
+        print(str(num) + " x " + str(number) + " = " + str(answer))
+        
+        #Increamenting the initiazation value
+        num = num+1
+elif(num<13):
+    while (num<13):
+        number = int(number)
+        #performing arthematics
+        answer = number * num
+        answer = int(answer)    
+        #displaying the times table of the user input number
+        print(str(num) + " x " + str(number) + " = " + str(answer))
+            
+        #Increamenting the initiazation value
+        num = num+1
+    else:
+        print("Thank you")
+else: 
+     print("Your input is not an integer")
